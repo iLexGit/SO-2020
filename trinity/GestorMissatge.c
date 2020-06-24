13dd8c69bfa768931210056972323a68fe094451
jvidalno <jvidalno@gmail.com>
2020-06-24 21:10:20 +0200

    Llargada ara funciona a nivell de bit i no amb representació hexadecimal

--- trinity/GestorMissatge.c
+++ trinity/GestorMissatge.c
@@ -4,17 +4,19 @@ char* generaTrama(int Opcio, char* data){
 
 	char type;
 	
-	char llargada [3];
+	char llargada [2];
 	char* String;
 
-	sprintf(llargada,"%X%X",(unsigned int)strlen(data)/16,(unsigned int)strlen(data)%16);
+    llargada[0] = strlen(data)/255;
+    llargada[1] = strlen(data)%255;
+
 
 	switch(Opcio){
 
 		case CON_CLI:
 			type = '1';
 			String = (char*)malloc(sizeof(char) * (1 + strlen(CON_CLI_HEADER) + 2 + strlen(data) + 2));
-			sprintf(String,"%c%s%s%s",type,CON_CLI_HEADER,llargada,data);
+			sprintf(String,"%c%s%c%c%s",type,CON_CLI_HEADER,llargada[0],llargada[1],data);
 
 		break;
 
@@ -27,7 +29,7 @@ char* generaTrama(int Opcio, char* data){
 		case CON_SER_KO:
 			type = '1';
 			String = (char*)malloc(sizeof(char) * (1 + strlen(CON_SER_KO_HEADER)));
-			sprintf(String,"%c%s",type,CON_CLI_HEADER);
+			sprintf(String,"%c%s%c%c",type,CON_CLI_HEADER,llargada[0],llargada[1]);
 
 		break;
 
